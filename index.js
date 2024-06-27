b545a0eeac6cbac1cd6513af1438295b1a20b339
NotMinhDucGamingTV <ntncar311@gmail.com>
2024-06-28 01:13:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const { Client, GatewayIntentBits } = require('discord.js');
 const app = express();
-const PORT = 15085;
+const PORT = process.env.PORT || 3000;
 
 // Discord Bot Setup
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
